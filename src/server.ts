6b22a350b02ef17eefff08df5461bedf1dbb9c0a
Manoel Duran <manoel.duran@hotmail.com>
2022-09-15 23:09:23 -0300

    created base backend with ts

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/ads', (request: Request, response: Response) => {
+    return response.json([
+        { id: 1, name: 'Anúncio 1' },
+        { id: 2, name: 'Anúncio 2' },
+        { id: 3, name: 'Anúncio 3' },
+        { id: 4, name: 'Anúncio 4' },
+    ])
+});
+
+app.listen(3337);
